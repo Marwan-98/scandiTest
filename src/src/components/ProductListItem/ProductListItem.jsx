4717accd5f99111c468006418818daeb940ed3a2
Marwan-98 <marwanashrafzaki98@gmail.com>
2024-07-15 20:19:03 +0300

    Transform ProductListItem to Links

--- src/src/components/ProductListItem/ProductListItem.jsx
+++ src/src/components/ProductListItem/ProductListItem.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import productImage from "./image.png";
 import cartImage from "./empty-cart.png";
 import "./ProductListItem.style.scss";
+import { Link } from "react-router-dom";
 
 class ProductListItem extends Component {
   render() {
     return (
-      <div className="ProductListItem">
+      <Link to={"/products/1"} className="ProductListItem">
         <img src={productImage} alt="product" width={354} height={330} />
         <button className="ProductListItem-AddToCartButton">
           <img src={cartImage} alt="add to cart" width={24} height={24} />
@@ -15,7 +16,7 @@ class ProductListItem extends Component {
           <h3 className="ProductListItem-Details-Title">Running Short</h3>
           <span className="ProductListItem-Details-Price">$50.00</span>
         </div>
-      </div>
+      </Link>
     );
   }
 }
