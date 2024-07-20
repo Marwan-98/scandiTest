c0a93536ce73f6fc1f33d59f89c024cd7cf6b544
Marwan-98 <marwanashrafzaki98@gmail.com>
2024-07-20 23:53:54 +0300

    Fixing Styling issues

--- src/src/components/PlusIcon/PlusIcon.jsx
+++ src/src/components/PlusIcon/PlusIcon.jsx
@@ -4,8 +4,8 @@ class PlusIcon extends Component {
   render() {
     return (
       <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5 1V9" stroke="#1D1F22" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M1 5H9" stroke="#1D1F22" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M5 1V9" stroke="#1D1F22" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M1 5H9" stroke="#1D1F22" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
     );
   }
